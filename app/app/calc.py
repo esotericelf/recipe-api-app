465aca8cd38ac1200843408e58a87f9d32fb1976
esotericelf <esoteric.elf@gmail.com>
2024-01-07 01:05:56 +0800

    fixed test.py and calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x, y):
+
     """ Add x and y and return results."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """ Subtract x from y """
     return y - x
-    
+
